5d07d58af48b8ee4777314b40d92576b60cafd12
JethroSama <kel73896@gmail.com>
2022-04-19 13:46:52 +0800

    select input buttons

---
+++ main.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+
+driver.get("https://www.deepl.com/translator#zh/en")
+
+driver.implicitly_wait(10)
+
+# set input language to chinese
+lang_input_element = driver.find_element(
+    by=By.CSS_SELECTOR, value="div.lmt__language_select--source>button")
+lang_input_element.click()
+
+# get button with attribute dl-test that contains translator-lang-option-zh
+lang_input_element = driver.find_element(
+    by=By.CSS_SELECTOR, value="button[dl-test='translator-lang-option-zh']")
+lang_input_element.click()
+
+# set output language to english
+
+# input_textarea = driver.find_element(
+#     by=By.CSS_SELECTOR, value='textarea.lmt__source_textarea')
+
+# input_textarea.send_keys("忙了好半天")
