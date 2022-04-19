7b33ef543f314da824260212beaa7afb83381499
JethroSama <kel73896@gmail.com>
2022-04-19 17:15:18 +0800

    change api structure

--- js/index.js
+++ js/index.js
@@ -5,16 +5,23 @@ import { getContent, getLinks } from "./scraper.js"
 const app = express()
 
 app.use(express.json())
-app.get("/chapter/:number", async (req, res) => {
+
+app.get("/chapters", async (req, res) => {
     // get params
-    const { number } = req.params
     const chapters = await getLinks('https://www.uukanshu.com/b/439/')
 
     // find chapter
-    const chapter = chapters.find(chapter => chapter.text.match(`第${number}章`))
+    // const chapter = chapters.find(chapter => chapter.text.match(`第${number}章`))
+    // get content
+    res.json(chapters)
+})
+
+app.get("/chapter", async (req, res) => {
+    // get params
+    const { url } = req.body
 
     // get content
-    const content = await getContent(chapter.href)
+    const content = await getContent(url)
     res.json(content)
 })
 
