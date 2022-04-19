4e6f332788c6d56eeecc6371b065ea2ec9b039c4
JethroSama <kel73896@gmail.com>
2022-04-19 13:48:59 +0800

    set output to english

--- main.py
+++ main.py
@@ -12,12 +12,18 @@ lang_input_element = driver.find_element(
     by=By.CSS_SELECTOR, value="div.lmt__language_select--source>button")
 lang_input_element.click()
 
-# get button with attribute dl-test that contains translator-lang-option-zh
 lang_input_element = driver.find_element(
     by=By.CSS_SELECTOR, value="button[dl-test='translator-lang-option-zh']")
 lang_input_element.click()
 
 # set output language to english
+lang_input_element = driver.find_element(
+    by=By.CSS_SELECTOR, value="div.lmt__language_select--target>button")
+lang_input_element.click()
+
+lang_input_element = driver.find_element(
+    by=By.CSS_SELECTOR, value="button[dl-test='translator-lang-option-en-US']")
+lang_input_element.click()
 
 # input_textarea = driver.find_element(
 #     by=By.CSS_SELECTOR, value='textarea.lmt__source_textarea')
