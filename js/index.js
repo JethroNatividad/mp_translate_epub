ee108bcafe86610f3a744b8bc8e704dfd283e553
JethroSama <kel73896@gmail.com>
2022-04-19 16:15:32 +0800

    setup createbook route

--- js/index.js
+++ js/index.js
@@ -2,12 +2,14 @@ import express from "express"
 
 const app = express()
 
+app.use(express.json())
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
 
 app.post("/createBook", (req, res) => {
-    const { text, author, title, imageUrl } = req.body
+    const { text, title, author, imageUrl, publisher } = req.body
+    res.send({ text, title, author, imageUrl, publisher })
 })
 
 app.listen(3000, () => {
